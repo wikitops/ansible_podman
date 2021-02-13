ccc4604c1dc36484c1a7739dec54eba88544bf1a
wikitops <wikitops5692@gmail.com>
2021-02-13 09:52:45 -0500

    refact(podman): update ansible config, set default python to python3

--- provision.sh
+++ provision.sh
@@ -4,7 +4,7 @@
 if [ -f /etc/redhat-release ] ; then
   sudo dnf update
   sudo dnf upgrade -y
-  sudo dnf install -y python36
+  sudo dnf install -y python3
 
 # Manage Debian / Ubuntu environment
 elif [ -f /etc/debian_version ] ; then
@@ -12,3 +12,5 @@ elif [ -f /etc/debian_version ] ; then
   sudo apt upgrade -y
   sudo apt install -y python3 python3-apt
 fi
+
+sudo alternatives --set python /usr/bin/python3
